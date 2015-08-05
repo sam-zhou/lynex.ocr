387bfc017bc16ea70452f6e652b195f2faabc844
Sam Zhou <samzhou.it@gmail.com>
2015-08-06 00:15:35 +0800

    added bill

---
+++ Lynex.Model/Domain/DbModels/Bill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lynex.Model.Enum;
+using Lynex.Model.Enum.Mapable;
+
+namespace Lynex.Model.Domain.DbModels
+{
+    public class Bill : BaseEntity
+    {
+        public virtual double Amount { get; set; }
+
+        public virtual DateTime? DueDate { get; set; }
+
+        public virtual DateTime? IssueDate { get; set; }
+
+        public virtual BillingCompany Company { get; set; }
+
+        public virtual BillType BillType { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
