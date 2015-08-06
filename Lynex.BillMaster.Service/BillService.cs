6542d00b92bed48e12b16e8c58468de57c5e273c
Sam Zhou <samzhouc@hotmail.com>
2015-08-06 11:46:18 +0800

    added missing files

---
+++ Lynex.BillMaster.Service/BillService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lynex.BillMaster.Model.Domain.DbModels;
+using Lynex.Common.Service;
+using Lynex.Database.Common;
+
+namespace Lynex.BillMaster.Service
+{
+    public class BillService:BaseService, IBillService
+    {
+        public BillService(IDatabaseService dbService) : base(dbService)
+        {
+        }
+
+        public IEnumerable<Bill> GetBillsForUser(User user)
+        {
+            return DatabaseService.GetAll<Bill>();
+        }
+
+        public Bill CreateBill(Bill bill, BillingCompany company, User user)
+        {
+            return bill;
+        }
+    }
+}
---
+++ Lynex.Model/Domain/DbModels/Mapping/EnumTableMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+
+namespace Lynex.BillMaster.Model.Domain.DbModels.Mapping
+{
+    public abstract class EnumTableMap<T> : ClassMap<EnumTable<T>> where T: struct , IConvertible
+    {
+        protected EnumTableMap()
+        {
+            Table(typeof(T).Name);
+            Id(q => q.Id).GeneratedBy.Assigned();
+            Map(q => q.Name).Length(20).Not.Nullable();
+        }
+    }
+
+}
